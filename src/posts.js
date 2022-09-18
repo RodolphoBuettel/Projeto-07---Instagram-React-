f5125e67bf4bbac9abc57f3b4f4e9d70acc1a6a2
Rodolpho Buettel <rodolfo@DESKTOP-C9SP90O.localdomain>
2022-09-18 15:01:40 -0300

    Like e Salvar feitos

--- src/posts.js
+++ src/posts.js
@@ -1,11 +1,36 @@
+import React from "react";
 
 function Post(props){
+    const[like, setLike] = React.useState("heart-outline");
+    const[clicado, setClicado] = React.useState("salvo");
+    const [salvar, setSalvar] = React.useState("bookmark-outline");
+    const [curtidas1, setCurtidas] = React.useState(props.curtidas);
+  
+    function curtir(){
+      if(like === "heart-outline"){
+        setLike("heart");
+        setClicado("red");
+        setCurtidas(curtidas1 + 1);
+      }else{
+        setLike("heart-outline");
+        setClicado("salvo")
+        setCurtidas(curtidas1 - 1);
+      }
+    }
+    function salvo(){
+      if(salvar === "bookmark-outline"){
+        setSalvar("bookmark");
+      }else{
+        setSalvar("bookmark-outline");
+      }
+    }
+
     return(
         <div class="post">
                 <div class="topo">
                     <div class="usuario">
                         <img src={props.imagemUsuario} />
-                        meowed
+                        {props.nome}
                     </div>
                     <div class="acoes">
                         <ion-icon name="ellipsis-horizontal"></ion-icon>
@@ -19,19 +44,19 @@ function Post(props){
                 <div class="fundo">
                     <div class="acoes">
                         <div>
-                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon onClick={curtir} name={like} role="img" class={clicado} aria-label={like}></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
                         <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
+                            <ion-icon onClick={salvo} name={salvar} role="img" class="md hydrated" aria-label={salvar}></ion-icon>
                         </div>
                     </div>
 
                     <div class="curtidas">
                         <img src={props.imagemCurtidas}/>
                         <div class="texto">
-                            {props.texto}
+                           Curtido por <strong>{props.texto}</strong> e <strong> outras {curtidas1}</strong>
                         </div>
                     </div>
                 </div>
@@ -42,15 +67,15 @@ function Post(props){
 
 export default function Posts() {
     const post = [
-        {imagemUsuario :"assets/img/meowed.svg" , imagemConteudo: "assets/img/gato-telefone.svg", imagemCurtidas :"assets/img/respondeai.svg" , texto: "Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>" },
-        {imagemUsuario :"assets/img/barked.svg" , imagemConteudo:"assets/img/dog.svg" , imagemCurtidas :"assets/img/adorable_animals.svg" , texto: "Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>" }
+        {imagemUsuario :"assets/img/meowed.svg" , nome: "meowed", imagemConteudo: "assets/img/gato-telefone.svg", imagemCurtidas :"assets/img/respondeai.svg" , texto: "respondeai", curtidas: 101523 },
+        {imagemUsuario :"assets/img/barked.svg" , nome: "barked", imagemConteudo:"assets/img/dog.svg" , imagemCurtidas :"assets/img/adorable_animals.svg" , texto: "adorable_animals", curtidas: 199159 }
     ]
 
 
     return (
         <div class="posts">
-            {post.map((p) => <Post imagemUsuario = {p.imagemUsuario} imagemConteudo = {p.imagemConteudo} 
-                imagemCurtidas = {p.imagemUsuario} texto = {p.texto}/> )}
+            {post.map((p) => <Post imagemUsuario = {p.imagemUsuario} nome = {p.nome} imagemConteudo = {p.imagemConteudo} 
+                imagemCurtidas = {p.imagemUsuario} texto = {p.texto} curtidas = {p.curtidas}/> )}
         </div>
 
     );
--- src/usuario.js
+++ src/usuario.js
@@ -1,13 +1,26 @@
+import React from "react";
+
 
 function Dados(props){
+    const [nome, setNome] = React.useState("Catana");
+    const [usuario, setUsuario] = React.useState("assets/img/catanacomics.svg");
+    function inserirNome(){
+        const nomePrompt = prompt("Qual o seu nome?");
+        setNome(nomePrompt);
+    }
+
+    function inserirImagem(){
+        const usuarioPrompt = prompt("Insira o link da imagem");
+        setUsuario(usuarioPrompt);
+    }
     return(
         <div class="usuario">
-            <img src={props.imagem} />
+            <img onClick={inserirImagem} src={usuario} />
             <div class="texto">
                 <strong>{props.texto}</strong>
                 <span>
-                   {props.span}
-                    <ion-icon name="pencil"></ion-icon>
+                   {nome}
+                    <ion-icon name="pencil" onClick = {inserirNome}></ion-icon>
                 </span>
             </div>
         </div>
@@ -17,7 +30,7 @@ function Dados(props){
 export default function Usuario() {
 
     return (
-        <Dados imagem = "assets/img/catanacomics.svg" texto = "catanacomics" span = "Catana" />
+        <Dados  texto = "catanacomics" />
     );
 
 }
